0263683ccb226db45d38843385209c7e97a712a7
MaximilianFinnican <MaximilianFinnican@gmail.com>
2018-01-31 15:53:54 -0500

    Card Class

---
+++ Card.java
@@ -0,0 +1,33 @@
+// This class handes the card functions.
+public class Card
+{
+  String suit;
+  String rank;
+
+  public Card(String suit, String rank)
+  {
+    this.suit = suit;
+    this.rank = rank;
+  }
+
+  public String getSuit()
+  {
+    return this.suit;
+  }
+
+  public void setSuit(String suit)
+  {
+    this.suit = suit;
+  }
+
+  public String getRank()
+  {
+    return this.rank;
+  }
+
+  /* Sets the rank of a card */
+  public void setRank(String rank)
+  {
+    this.rank = rank;
+  }
+}

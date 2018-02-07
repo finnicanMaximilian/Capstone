97cf08d5fd7b480e02417c73556a29dd9c6541ba
MaximilianFinnican <MaximilianFinnican@gmail.com>
2018-02-07 17:11:17 -0500

    Still learning how to use Java FX but at this point i figured out how to color the background, and post pictures of card images representing a basic idea of how i inivsioned the computers hand to look along with the deck in the middle of the application.

--- GuiTest.java
+++ GuiTest.java
@@ -1,4 +1,5 @@
 
+
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
@@ -11,6 +12,9 @@ import javafx.scene.paint.LinearGradient;
 import javafx.scene.paint.RadialGradient;
 import javafx.scene.paint.Stop;
 import javafx.stage.Stage;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+ 
 
 
 public class GuiTest extends Application 
@@ -29,10 +33,30 @@ public class GuiTest extends Application
         Group root = new Group();
         Canvas canvas = new Canvas(1200, 900);
         GraphicsContext gc = canvas.getGraphicsContext2D();
+        
+        drawBoard(gc);
+        
         root.getChildren().add(canvas);
         primaryStage.setScene(new Scene(root));
+        gc.setFill(Color.GREEN);
         primaryStage.show();
 		return;
+	}
+	
+	public void drawBoard(GraphicsContext gc)
+	{
+        gc.setFill(Color.GREEN);
+        gc.fillRoundRect(0, 0, 1200, 900, 0, 0);
+        Image cardImage = new Image("cheetah-card.gif");
+        gc.drawImage(cardImage, 1040, 8, 150, 200);
+        gc.drawImage(cardImage, 890, 8, 150, 200);
+        gc.drawImage(cardImage, 735, 8, 150, 200);
+        gc.drawImage(cardImage, 580, 8, 150, 200);
+        gc.drawImage(cardImage, 425, 8, 150, 200);
+        
+        gc.drawImage(cardImage, 540, 350, 150, 200);
+
+
 	}
 
 }

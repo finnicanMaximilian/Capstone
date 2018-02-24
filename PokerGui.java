11a79c8c235779678f292c8a7eb14790da93f9ad
MaximilianFinnican <MaximilianFinnican@gmail.com>
2018-02-23 21:03:35 -0500

    Found where the bug is.. now time to re-engineer the code..

--- PokerGui.java
+++ PokerGui.java
@@ -63,6 +63,7 @@ public class PokerGui extends Application implements EventHandler<ActionEvent>
 	String listOfCards = "";
 	int numberOfCards = 0;
 	Text playerWin = new Text();
+	ObservableList<String> cards;
 	
 
 	
@@ -74,15 +75,15 @@ public class PokerGui extends Application implements EventHandler<ActionEvent>
 	@Override
 	public void start(Stage primaryStage) throws Exception 
 	{
-		giveBackText.setVisible(false);
-		window = primaryStage;
-        window.setTitle("Poker Fanatic!");
+		this.giveBackText.setVisible(false);
+		this.window = primaryStage;
+        this.window.setTitle("Poker Fanatic!");
         Button playButton = new Button("Play Poker!");
         Button showWin = new Button("See who Won!");
         Button giveBack = new Button("Give Back Cards");
 
-        gc.setFill(Color.GREEN);
-        gc.fillRoundRect(0, 0, 1200, 900, 0, 0);
+        this.gc.setFill(Color.GREEN);
+        this.gc.fillRoundRect(0, 0, 1200, 900, 0, 0);
         
 
         showWin.setVisible(false);
@@ -121,20 +122,20 @@ public class PokerGui extends Application implements EventHandler<ActionEvent>
         playButton.setLayoutY(350);
 
         playButton.setOnAction(e -> {
-        	playerWin.setVisible(false);
+        	this.playerWin.setVisible(false);
         	launchPoker();
             Collections.shuffle(theDeck);
         	dealCards();
         	playButton.setVisible(false);
         	giveBack.setVisible(true);
-        	giveBackText.setVisible(true);
+        	this.giveBackText.setVisible(true);
         });
 
-        root.getChildren().addAll(canvas, playButton, giveBack, giveBackText, showWin);
+        this.root.getChildren().addAll(canvas, playButton, giveBack, giveBackText, showWin);
         
         
         // create scene with root.
-        scene = new Scene(root);
+        this.scene = new Scene(root);
         
 		// Adding the CSS sheet
 		URL url = this.getClass().getResource("Poker.css");
@@ -143,19 +144,18 @@ public class PokerGui extends Application implements EventHandler<ActionEvent>
 			System.exit(-1);
 		}
 		String css = url.toExternalForm(); 
-		scene.getStylesheets().add(css);
+		this.scene.getStylesheets().add(css);
 		
 		// add scene to window.
-        window.setScene(scene);
-        gc.setFill(Color.GREEN);
-        window.show();
+        this.window.setScene(this.scene);
+        this.gc.setFill(Color.GREEN);
+        this.window.show();
 		return;
 	}
 	
 	@Override
 	public void handle(ActionEvent event)
 	{
-
 		return;
 	}
 	
@@ -165,56 +165,54 @@ public class PokerGui extends Application implements EventHandler<ActionEvent>
 	public void dealCards()
 	{
         // Picture of Deck
-        gc.drawImage(cardImage, 540, 350, 150, 200);
+        this.gc.drawImage(cardImage, 540, 350, 150, 200);
 		// j is used for spacing for Computer Card's X Coordinate
 		int j = 1040;
 		// m is used for spacing for Player Card's X Coordinate
 		int m = 10;
 		// aligns the view in horizontal
-		listView.setOrientation(Orientation.HORIZONTAL);
+		this.listView.setOrientation(Orientation.HORIZONTAL);
 		for(int i = 0; i < 5; i++)
 		{
-			player.setCard(theDeck.get(i));
+			this.player.setCard(theDeck.get(i));
 
 			// Adding name to cards to list view, this can later be used to give back
 			// String names of cards in order to feed into player.java's search hand method.
-			listView.getItems().add((player.hand.get(i).getRank() + " " 
+			this.listView.getItems().add((player.hand.get(i).getRank() + " " 
 					+ player.hand.get(i).getSuit()));	
 			m = m + 160;
 		}
 		for(int i = 0; i < 5; i++)
 		{
-			theDeck.remove(0);
+			this.theDeck.remove(0);
 		}
 		// Drawing opponents hand.
 		for(int i = 0; i < 5; i++)
 		{
-	        gc.drawImage(cardImage, j, 8, 150, 200);
+	        this.gc.drawImage(cardImage, j, 8, 150, 200);
 	        j = j - 155;
-			opponent.setCard(theDeck.get(i));
+			this.opponent.setCard(theDeck.get(i));
 		}
 		for(int i = 0; i < 5; i++)
 		{
-			theDeck.remove(0);
+			this.theDeck.remove(0);
 		}
 		// Multiple Cards can be selected
-		listView.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
-		listView.setPadding(new Insets(10,10,10,10));
-		listView.setPrefSize(550, 200);
-		listView.setLayoutX(10);
-		listView.setLayoutY(600);
-		//listView.setMinHeight(150);
-		//listView.setMinWidth(800);
-		root.getChildren().add(listView);
+		this.listView.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
+		this.listView.setPadding(new Insets(10,10,10,10));
+		this.listView.setPrefSize(550, 200);
+		this.listView.setLayoutX(10);
+		this.listView.setLayoutY(600);
+		this.root.getChildren().add(listView);
 	}
 	
 	public void drawBoard(GraphicsContext gc)
 	{
         // Making the title of the game.
-        gc.setFill(Color.WHITE);
-        gc.fillRoundRect(15, 15, 350, 180, 5, 5);
-        gc.setFill(Color.BLACK);
-        gc.strokeText("Poker Fanatic", 140, 110);
+		this.gc.setFill(Color.WHITE);
+		this.gc.fillRoundRect(15, 15, 350, 180, 5, 5);
+		this.gc.setFill(Color.BLACK);
+		this.gc.strokeText("Poker Fanatic", 140, 110);
 	}
 	
 	/*
@@ -222,14 +220,13 @@ public class PokerGui extends Application implements EventHandler<ActionEvent>
 	 */
 	private void buttonClicked()
 	{
-		listOfCards = "";
-		ObservableList<String> cards;
-		cards = listView.getSelectionModel().getSelectedItems();
+		this.listOfCards = "";
+		this.cards = listView.getSelectionModel().getSelectedItems();
 		
-		for(String m: cards)
+		for(String m: this.cards)
 		{
-			listOfCards += m + "\n";
-			numberOfCards++;
+			this.listOfCards += m + "\n";
+			this.numberOfCards++;
 		}
 		//System.out.println(message);
 		return;
@@ -249,16 +246,15 @@ public class PokerGui extends Application implements EventHandler<ActionEvent>
 	 */
 	public void initiateGiveBack(int person)
 	{
-		root.getChildren().remove(listView);
+		this.root.getChildren().remove(listView);
 		if(person == 0)
 		{
-			//System.out.println("How many cards would you like to give back?");
-			Scanner keyboard = new Scanner(listOfCards);
+			Scanner keyboard = new Scanner(this.listOfCards);
 			int numOfCards = this.numberOfCards;
-			if(numOfCards == 4 && ((player.searchHand("Ace", "Clubs") != 5)
-					|| (player.searchHand("Ace", "Hearts") != 5)
-					|| (player.searchHand("Ace", "Spades") != 5)
-					|| (player.searchHand("Ace", "Diamonds") != 5)))
+			if(numOfCards == 4 && ((this.player.searchHand("Ace", "Clubs") != 5)
+					|| (this.player.searchHand("Ace", "Hearts") != 5)
+					|| (this.player.searchHand("Ace", "Spades") != 5)
+					|| (this.player.searchHand("Ace", "Diamonds") != 5)))
 			{
 
 				// Ace is present within the Persons hand
@@ -266,53 +262,45 @@ public class PokerGui extends Application implements EventHandler<ActionEvent>
 				{
 					String rank = keyboard.next();
 					String suit = keyboard.next();
-					int cNum = player.searchHand(rank, suit);
-					player.giveBack(cNum);
-					// Seeing if the item gets removed from the list.
-					listView.getItems().remove(cNum);
-					
-					
-					
+					int cNum = this.player.searchHand(rank, suit);
+					this.player.giveBack(cNum);
+					this.listView.getItems().remove(cNum);
 				}
-
-				//System.out.println("\n\nGiving Back Cards....");
 				for(int i = 0; i < numOfCards; i++)
 				{
 					String rank = theDeck.get(i).getRank();
 					String suit = theDeck.get(i).getSuit();
 					Card newCard = new Card(suit, rank);
-					player.setCard(newCard);
-					listView.getItems().add(rank + " " + suit);
+					this.player.setCard(newCard);
+					this.listView.getItems().add(rank + " " + suit);
 				}
 				for(int i = 0; i < numOfCards; i++)
 				{
-					theDeck.remove(0);
+					this.theDeck.remove(0);
 				}
 			}
 			else if(numOfCards < 4 && numOfCards != 0)
 			{
-//				System.out.println("Please type the Rank and Suit of the card you would like to give back\n"
-//						+ "			Then press ENTER after each card.");
 				for(int i = 0; i < numOfCards; i++)
 				{
 					String rank = keyboard.next();
 					String suit = keyboard.next();
 					int cNum = player.searchHand(rank, suit);
-					player.giveBack(cNum);
+					this.player.giveBack(cNum);
 					// Seeing if the item gets removed from the list.
-					listView.getItems().remove(cNum);
+					this.listView.getItems().remove(cNum);
 				}
 				for(int i = 0; i < numOfCards; i++)
 				{
-					String rank = theDeck.get(i).getRank();
-					String suit = theDeck.get(i).getSuit();
+					String rank = this.theDeck.get(i).getRank();
+					String suit = this.theDeck.get(i).getSuit();
 					Card newCard = new Card(suit, rank);
-					player.setCard(newCard);
-					listView.getItems().add(rank + " " + suit);
+					this.player.setCard(newCard);
+					this.listView.getItems().add(rank + " " + suit);
 				}
 				for(int i = 0; i < numOfCards; i++)
 				{
-					theDeck.remove(0);
+					this.theDeck.remove(0);
 				}
 			}
 			else if(numOfCards == 0)
@@ -334,11 +322,11 @@ public class PokerGui extends Application implements EventHandler<ActionEvent>
 			int numOfCards = opponent.think(0);
 			for(int i = 0; i < numOfCards; i++)
 			{
-				opponent.setCard(theDeck.get(i));
+				this.opponent.setCard(this.theDeck.get(i));
 			}
 			for(int i = 0; i < numOfCards; i++)
 			{
-				theDeck.remove(0);
+				this.theDeck.remove(0);
 			}
 
 		}
@@ -346,7 +334,7 @@ public class PokerGui extends Application implements EventHandler<ActionEvent>
 		{
 			System.out.println("Wrong number entered, please enter 0 for player and 1 for opponent.");
 		}
-		root.getChildren().add(listView);
+		this.root.getChildren().add(this.listView);
 		return;
 	}
 	
@@ -359,18 +347,18 @@ public class PokerGui extends Application implements EventHandler<ActionEvent>
 		for(int i = 0; i < 5; i++)
 		{
 			// Draw Over Computer Cards With It's Cards.
-			gc.setFill(Color.WHITE);
-			gc.fillRoundRect(j, 8, 150, 200, 8, 8);
-			gc.setFill(Color.BLACK);
-			gc.fillText((opponent.hand.get(i).getRank() + " " + opponent.hand.get(i).getSuit()), (j+10), 100);
+			this.gc.setFill(Color.WHITE);
+			this.gc.fillRoundRect(j, 8, 150, 200, 8, 8);
+			this.gc.setFill(Color.BLACK);
+			this.gc.fillText((opponent.hand.get(i).getRank() + " " + opponent.hand.get(i).getSuit()), (j+10), 100);
 			j = j - 155;
 		}	
-		player.calcHand();
-		opponent.calcHand();
-		gc.fillText(("Total Player Points: " +player.winPoints), 600, 700);
-		gc.fillText(("Total Computer Points: " + opponent.winPoints), 175, 150);
-		gc.fillText(("Highest Ranked Player Card: " + player.highCard), 600, 750);
-		gc.fillText(("Highest Ranked Computer Card: " + opponent.highCard), 175, 200);
+		this.player.calcHand();
+		this.opponent.calcHand();
+		this.gc.fillText(("Total Player Points: " +player.winPoints), 600, 700);
+		this.gc.fillText(("Total Computer Points: " + opponent.winPoints), 175, 150);
+		this.gc.fillText(("Highest Ranked Player Card: " + player.highCard), 600, 750);
+		this.gc.fillText(("Highest Ranked Computer Card: " + opponent.highCard), 175, 200);
 			
 		return;
 	}
@@ -383,46 +371,49 @@ public class PokerGui extends Application implements EventHandler<ActionEvent>
 		this.player = new Player();
 		this.opponent = new Opponent();
 		// Trying to re-instantiate a players hand.
-    	this.listView = new ListView<>();
+		// the listView is populated before this method.. so when i create a new listView its removing the list
+		// before buttonClicked() can establish what is inside the hand.
+		for(int i = 0; i < 5; i++)	this.listView.getItems().remove(i);
+
 	}
 	
 	public void printWinner()
 	{
 
-		playerWin.setLayoutX(500);
-		playerWin.setLayoutY(450);
-		playerWin.setVisible(false);
+		this.playerWin.setLayoutX(500);
+		this.playerWin.setLayoutY(450);
+		this.playerWin.setVisible(false);
 
-		root.getChildren().add(playerWin);
-        gc.setFill(Color.GREEN);
-        gc.fillRoundRect(0, 0, 1200, 900, 0, 0);
-        gc.setFill(Color.BLACK);
-        root.getChildren().remove(listView);
+		this.root.getChildren().add(playerWin);
+		this.gc.setFill(Color.GREEN);
+		this.gc.fillRoundRect(0, 0, 1200, 900, 0, 0);
+		this. gc.setFill(Color.BLACK);
+		this. root.getChildren().remove(listView);
         // Player Won.
-        if(player.winPoints > opponent.winPoints)
+        if(this.player.winPoints > this.opponent.winPoints)
         {
-        	playerWin.setText("Player Won");
+        	this.playerWin.setText("Player Won");
         }
-        else if(opponent.winPoints > player.winPoints)
+        else if(this.opponent.winPoints > this.player.winPoints)
         {
-        	playerWin.setText("Computer Won!");
+        	this.playerWin.setText("Computer Won!");
         }
-        else if(opponent.winPoints == player.winPoints)
+        else if(this.opponent.winPoints == this.player.winPoints)
         {
-        	if(opponent.highCard > player.highCard)
+        	if(this.opponent.highCard > this.player.highCard)
         	{
-        		playerWin.setText("Computer Won with High Card!");
+        		this.playerWin.setText("Computer Won with High Card!");
         	}
         	else if(player.highCard > opponent.highCard)
         	{
-        		playerWin.setText("Player Won with High Card!");
+        		this.playerWin.setText("Player Won with High Card!");
         	}
         	else
         	{
-        		playerWin.setText("Tie Game!!");
+        		this.playerWin.setText("Tie Game!!");
         	}
         }
-     	playerWin.setVisible(true);
+        this.playerWin.setVisible(true);
         return;
 	}
 
